cb356ffdc5d709adf406838d70a068b92c45ad94
Javier GM <javiergranadosmartin@gmail.com>
2024-02-15 12:39:17 +0100

    first commit

---
+++ src/app/country/services/countries.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Country, Region, SmallCountry } from '../interfaces/country.interface';
+import { Observable, combineLatest, map, of, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountriesService {
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
+  private _regions: Region[] = [
+    Region.africa,
+    Region.america,
+    Region.asia,
+    Region.europe,
+    Region.oceania,
+  ];
+
+  constructor(private http: HttpClient) {}
+
+  get regions(): Region[] {
+    return [...this._regions];
+  }
+
+  getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
+    if (!region) return of([]);
+
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) =>
+        countries.map((country) => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? [],
+        }))
+      )
+    );
+  }
+
+  getCountryByAlphaCode(alphaCode: string): Observable<SmallCountry> {
+    const url: string = `${this.baseUrl}/alpha/${alphaCode}?fields=cca3,name,borders`;
+
+    return this.http.get<Country>(url).pipe(
+      map((country) => ({
+        name: country.name.common,
+        cca3: country.cca3,
+        borders: country.borders ?? [],
+      }))
+    );
+  }
+
+  getCountryBordersByCodes(borders: string[]): Observable<SmallCountry[]> {
+    if (!borders || borders.length === 0) return of([]);
+
+    const countryRequests: Observable<SmallCountry>[] = [];
+
+    borders.forEach((code) => {
+      const request = this.getCountryByAlphaCode(code);
+      countryRequests.push(request);
+    });
+
+    return combineLatest(countryRequests);
+  }
+}
